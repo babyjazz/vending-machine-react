4b0ad7541cf01b3201929a3d6ae5fc53214e09c1
Jitiwatana Robru <jitiwattana@satang.com>
2020-04-11 21:44:31 +0700

    :hammer: update create action

--- src/utils/createActions.js
+++ src/utils/createActions.js
@@ -1,21 +1,33 @@
 import { createAction } from "@reduxjs/toolkit";
 import mapValues from "lodash/mapValues";
+import isPlainObject from "lodash/isPlainObject";
 
 export const requestActions = {
   request: "REQUEST",
   success: "SUCCESS",
   failure: "FAILURE",
+  reset: "RESET",
 };
 
-export function createActions(prefix, actions) {
+/**
+ *
+ * @param prefix: name of action as a prefix
+ * @param actions: action(Verb) of prefix action(Noun) name
+ * @return action: full action type name Ex. "PREFIX_ACTION"
+ */
+export function createActions(prefix, actions = requestActions) {
   const object = mapValues(actions, (type) => {
     return `${prefix}_${type}`;
   });
 
-  const mappedAction = {
-    request: createAction(object.request),
-    success: createAction(object.success),
-    failure: createAction(object.failure),
-  };
-  return mappedAction
+  const mappedAction = Object.keys(actions).reduce((prev, curr) => {
+    if (!isPlainObject(prev)) {
+      return {
+        [prev]: createAction(object[prev]),
+        [curr]: createAction(object[curr]),
+      };
+    }
+    return { ...prev, [curr]: createAction(object[curr]) };
+  });
+  return mappedAction;
 }
