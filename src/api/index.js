6eec29027d45785d85215f3bb92d0a0e3150c996
Jitiwatana Robru <jitiwattana@satang.com>
2020-04-12 13:54:19 +0700

    :pencil: update readme & :mute: remove log

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,6 @@ import request from 'utils/request'
 
 export async function listUserRequest({ userId }) {
   if (userId) {
-    console.log('debug ', userId)
     return request(`/users/${userId}?delay=1`)
   } else {
     return request('/users?delay=1')
